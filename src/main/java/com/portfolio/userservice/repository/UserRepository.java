c11ff89bbb88ab76c692238e579615bdbe355e0e
Yago Maia <yagogmaia@gmail.com>
2021-11-30 19:36:32 -0300

    Criação do UserRepository

---
+++ src/main/java/com/portfolio/userservice/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.portfolio.userservice.repository;
+
+import com.portfolio.userservice.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
